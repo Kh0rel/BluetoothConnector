875565c6d52a8936e969e587775b6d01a579d636
shemana <guillaumeChb@MacBook-Air-de-guillaume-2.local>
2016-01-21 00:32:00 +0100

    Inital commit
    Inital commit
    added BLE framework by RedBearLab

---
+++ BluetoothConnector/SHHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHHomeViewController.h
+//  BluetoothConnector
+//
+//  Created by guillaume chieb bouares on 21/01/2016.
+//  Copyright (c) 2016 me.shemana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHHomeViewController : UIViewController
+
+@end
---
+++ BluetoothConnector/SHHomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  SHHomeViewController.m
+//  BluetoothConnector
+//
+//  Created by guillaume chieb bouares on 21/01/2016.
+//  Copyright (c) 2016 me.shemana. All rights reserved.
+//
+
+#import "SHHomeViewController.h"
+
+@interface SHHomeViewController ()
+
+@end
+
+@implementation SHHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
