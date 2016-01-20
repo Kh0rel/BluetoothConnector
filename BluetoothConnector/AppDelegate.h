b251c49fabf3ec3176a0fd1b0adfe3d876d0d7b2
shemana <guillaumeChb@MacBook-Air-de-guillaume-2.local>
2016-01-20 16:52:02 +0100

    Initial Commit

---
+++ BluetoothConnector/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BluetoothConnector
+//
+//  Created by guillaume chieb bouares on 20/01/2016.
+//  Copyright (c) 2016 me.shemana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BluetoothConnectorTests/BluetoothConnectorTests.m
@@ -0,0 +1,40 @@
+//
+//  BluetoothConnectorTests.m
+//  BluetoothConnectorTests
+//
+//  Created by guillaume chieb bouares on 20/01/2016.
+//  Copyright (c) 2016 me.shemana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface BluetoothConnectorTests : XCTestCase
+
+@end
+
+@implementation BluetoothConnectorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
